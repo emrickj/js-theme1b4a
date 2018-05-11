7c851e38d1e75b6c873b847080f7e20ca268ef0d
John Emrick <emrickj248@comcast.net>
2018-05-11 16:11:49 -0400

    Add files via upload

--- scripts/theme1b4a.js
+++ scripts/theme1b4a.js
@@ -5,78 +5,69 @@ if (pos != -1) p = url.charAt(pos+2); else p = "1";
 pos = url.search("w=[1-9]");
 if (pos != -1) w = url.charAt(pos+2); else w = "1";
 
-$(function(){
-	$.ajax({
-	type      : "GET",
-	url       : "data/website.xml",
-	dataType  : "xml",
-	beforeSend: function(){
-	    oc = $(".card-body").html();
-	},
-	success   : function(xmlData){
-		xml=xmlData;
-		var title = $("title",xml).text();
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		xml = this.responseXML;
+		var x = xml.getElementsByTagName("title")[0].childNodes[0];
+		if (x) var title = x.nodeValue; else title = "";
 		pos=title.lastIndexOf("> ");
 		if (pos != -1) title=title.substring(pos+2);
 		pos=title.lastIndexOf(" <");
 		if (pos != -1) title=title.substring(0,pos);
-		$("title").text(title);
-		title = $("title",xml).text();
-		$(".navbar-brand").html(title);
-		$("#title").find("center").html(title);
-		var stl = $("style",xml).text();
-		$("style").append(stl);
+		document.getElementsByTagName("title")[0].innerText=title;
+		if (x) title = x.nodeValue; else title = "";
+		document.getElementById("nbtitle").innerHTML=title;
+		document.getElementById("title").innerHTML = "<b><h2><center>" + title + "</center></h2></b>";
+		var x = xml.getElementsByTagName("style")[0].childNodes[0];
+		if (x) var stl = x.nodeValue; else stl = "";
+		var ist = document.getElementsByTagName("style")[0].innerText;
+		document.getElementsByTagName("style")[0].innerText = ist + stl;
 		var pn = "", pn2 = "";
-		$("name:not(:empty)",xml).each(function(i,v){
+		var pnames = xml.evaluate("/website/page/name[.!='']", xml, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
+		for ( var i=0 ; i < pnames.snapshotLength; i++ ) {
 		   pn += "<li class='nav-item'><a class='nav-link' href='javascript:render(" + (i+1) + ",1);'>"
-		   + $(v).text() + "</a></li>";
+		   + pnames.snapshotItem(i).textContent + "</a></li>";
 		   pn2 += "<a href='javascript:render(" + (i+1) + ",1);' class='btn btn-outline-primary'>"
-		   + $(v).text() + "</a>";
-		});
-		$(".navbar-nav").html(pn);
-		$(".btn-group-vertical").prepend(pn2);
-	},
-	error     : function(){
-		alert("Could not retrieve XML file.");
-	},
-	complete  : function(){
+		   + pnames.snapshotItem(i).textContent + "</a>";
+		}
+		document.getElementById("myNavbar").firstElementChild.innerHTML = pn;
+		document.getElementById("btngrp").insertAdjacentHTML("afterbegin",pn2);
 		loadAnotherFile();
 	}
-	 });
-});
-
+};
+xhttp.open("GET", "data/website.xml", true);
+xhttp.send();
+  
 function loadAnotherFile() {
-	$.ajax({
-	type      : "GET",
-	url       : "data/website2.xml",
-	dataType  : "xml",
-	success   : function(xmlData){
-		xml2=xmlData;
-		var title = $("title",xml2).text();
-		$(".dropdown-toggle").prepend(title);
-		var pn = "";
-		$("name:not(:empty)",xml2).each(function(i,v){
-		   pn += "<li><a class='dropdown-item' href='javascript:render(" + (i+1) + ",2);'>" 
-		   + $(v).text() + "</a></li>";
-		});
-		$(".dropdown-menu").html(pn);
-	},
-	error     : function(){
-		alert("Could not retrieve XML file.");
-	},
-	complete  : function(){
-	    btn = $(".navbar-nav").html();
-		btn2 = $(".btn-group-vertical").html();
-		render(p,w);
-	}
-	 });
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			xml2 = this.responseXML;
+			var x = xml2.getElementsByTagName("title")[0].childNodes[0];
+			var title = x.nodeValue;
+			document.getElementById("btngrp").lastElementChild.insertAdjacentHTML("afterbegin",title);
+			var pn = "";
+			var pnames = xml2.evaluate("/website/page/name[.!='']", xml2, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
+			for ( var i=0 ; i < pnames.snapshotLength; i++ ) {
+			   pn += "<li><a class='dropdown-item' href='javascript:render(" + (i+1) + ",2);'>" 
+			   + pnames.snapshotItem(i).textContent + "</a></li>";
+			}
+			document.getElementById("ddmenu").innerHTML = pn;
+			btn = document.getElementById("myNavbar").firstElementChild.innerHTML;
+			btn2 = document.getElementById("btngrp").innerHTML;
+			render(p,w);
+		}
+	};
+	xhttp.open("GET", "data/website2.xml", true);
+	xhttp.send();
 }
 
 function render(pn, ws) {
     if (ws!=1) sf=xml2; else sf=xml;
-    var cp = $("page:eq("+(pn-1)+")",sf);
-	img=cp.find("image").text();
-	cnt=cp.find("contents").text();
+	var img = sf.evaluate('/website/page['+pn+']/image', sf, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null );
+	var contents = sf.evaluate('/website/page['+pn+']/contents', sf, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null );
+	var cnt = contents.singleNodeValue.textContent;
     while (cnt.includes('"?p=')) {
 	   cnt=cnt.replace('"?p=1','"javascript:render(1,'+ws+');');
 	   cnt=cnt.replace('"?p=2','"javascript:render(2,'+ws+');');
@@ -85,27 +76,30 @@ function render(pn, ws) {
 	   cnt=cnt.replace('"?p=5','"javascript:render(5,'+ws+');');
 	   cnt=cnt.replace('"?p=6','"javascript:render(6,'+ws+');');
     }
-	$(".card-img-top").attr("src",img);
-	if (cp.attr("type")=="comments")
-	   cnt = '<div id="HCB_comment_box" style="background-color: transparent;">' +
-	   '<a href="https://www.htmlcommentbox.com">HTML Comment Box</a> is loading comments...</div>' +
-	   '<link rel="stylesheet" type="text/css" href="https://www.htmlcommentbox.com/static/skins/default/skin.css" />' +
-	   '\u003cscript type="text/javascript" language="javascript" id="hcb">' +
-	   'if(!window.hcb_user){hcb_user={  };} (function(){s=document.createElement("script");s.setAttribute("type","text/javascript");s.setAttribute("src", "https://www.htmlcommentbox.com/jread?page="+escape((window.hcb_user && hcb_user.PAGE)||(""+window.location)).replace("+","%2B")+"&opts=470&num=10");' +
-	   'if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})();\u003c/script>';
-	$(".card-body").html(oc);
-	if (cp.attr("type")=="form") $(".form-horizontal").show();
-	$(".card-body").prepend(cnt);
-	$(".navbar-nav").html(btn);
-	$(".btn-group-vertical").html(btn2);
+	document.getElementById("pimage").setAttribute("src", img.singleNodeValue.textContent);
+	var attr = sf.evaluate('/website/page['+pn+']/@type', sf, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null );
+	document.getElementById("pbody").innerHTML = cnt;
+	if (attr.singleNodeValue.textContent=="form")
+		document.getElementById("ctform").style.display = "block";
+	else document.getElementById("ctform").style.display = "none";
+	if (attr.singleNodeValue.textContent=="comments") 
+		document.getElementById("pcmts").style.display = "block";
+	else document.getElementById("pcmts").style.display = "none";
+	document.getElementById("myNavbar").firstElementChild.innerHTML = btn;
+	document.getElementById("btngrp").innerHTML = btn2;
 	if (ws==1) {
-	   $(".navbar .nav-link:eq("+(pn-1)+")").addClass("active");
-	   $(".btn-group-vertical .btn:eq("+(pn-1)+")").addClass("active");
+	   var x = document.getElementById("myNavbar");
+	   x.getElementsByTagName("a")[pn-1].className += " active";
+	   x = document.getElementById("btngrp");
+	   x.getElementsByTagName("a")[pn-1].className += " active";
 	}
-	if (ws==2)
-       $(".dropdown-menu .dropdown-item:eq("+(pn-1)+")").addClass("active");	   
-    $("a").each(function(){
-	   var pn =$(this).text();
-	   if (pn.charAt(1)==" ") $(this).html('<i class="fa">'+pn.charAt(0)+'</i>'+pn.substring(1));
-	});
+	if (ws==2) {
+	   var x = document.getElementById("ddmenu");
+	   x.getElementsByTagName("a")[pn-1].className += " active";
+	}
+	var nl = document.getElementsByTagName("a").length;
+	for (var i=0;i<nl;i++) {
+	   var pn = document.getElementsByTagName("a")[i].innerText;
+	   if (pn.charAt(1)==" ") document.getElementsByTagName("a")[i].innerHTML = '<i class="fa">'+pn.charAt(0)+'</i>'+pn.substring(1);
+	}
 }
